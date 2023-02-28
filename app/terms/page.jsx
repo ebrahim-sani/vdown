2f21e09fe758b8f03366498f0b4d72da4cf48429
ebrahim-sani <ibraheemsani035@gmail.com>
2023-02-28 10:16:57 +0100

    ...term

--- app/terms/page.jsx
+++ app/terms/page.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "../Navbar";
+import TermContent from "./TermContent";
 
 const page = () => {
-   return <div>this terms page</div>;
+   return (
+      <main className="flex flex-col justify-between px-[1rem] md:px-[6rem] py-[2rem] min-h-[100vh]">
+         <Navbar />
+         <TermContent />
+      </main>
+   );
 };
 
 export default page;
